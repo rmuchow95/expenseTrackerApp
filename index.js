1dafacbb17834e83a7673d4b851ef3bd6e83eda1
rmuchow95 <rachelmuchow95@gmail.com>
2022-09-18 09:26:18 -0500

    Adjusted CSS, added JS

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const form = document.getElementById("textEntry");
+
+function addTableRow(e) {
+  e.preventDefault();
+  // let table = document.getElementById("table-container");
+  // let tableRow = table.insertRow(0);
+  console.log("click");
+}
+
+const submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", addTableRow);
